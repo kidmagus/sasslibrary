7f441d92ec15035e01f191c97b4ee26bada95b2a
kidmagus <msamson19@Outlook.com>
2024-05-01 21:48:42 +0800

    SassLib

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-
+//must use in terminal
+//npm install gulp-purgecss --save-dev
+//then ^C and gulp
+//const purgecss = require('gulp-purgecss')
 function buildStyles() {
-  return src("project/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return (
+    src("scss-files/**/*.scss")
+      .pipe(sass())
+      //.pipe(purgecss({content: ['*.html']}))
+      .pipe(dest("css"))
+  );
 }
 
 function watchTask() {
-  watch(["project/**/*.scss"], buildStyles);
+  watch(["scss-files/**/*.scss" /*,  "*.html"*/], buildStyles);
 }
+//all comments to lower the file size on index.css
 
 exports.default = series(buildStyles, watchTask);
